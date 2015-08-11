b376e3801b9b756db5c53bc29232e7a2dce08cb5
Jeff Lindsay <progrium@gmail.com>
2015-08-11 12:49:49 -0500

    using PrintEnv

--- env.go
+++ env.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"os"
 
 	"github.com/progrium/envconfig"
 	"github.com/progrium/hostctl/providers"
@@ -24,33 +23,10 @@ var envCmd = &cobra.Command{
 	Use:   "env",
 	Short: "Shows current relevant environment",
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, v := range envconfig.Vars() {
-			printVar(v)
-		}
+		envconfig.PrintEnv(os.Stdout, exportMode, secretsMode)
 		provider, _ := providers.Get(providerName, false)
 		if provider != nil {
-			for _, v := range provider.Env().Vars() {
-				printVar(v)
-			}
+			provider.Env().PrintEnv(os.Stdout, exportMode, secretsMode)
 		}
 	},
-}
-
-func printVar(out io.Writer, v *envconfig.ConfigVar) {
-	value := v.Value.String()
-	if v.Secret {
-		if secretsMode {
-			value = v.Value.Get().(string)
-		} else {
-			if exportMode {
-				return
-			}
-		}
-	}
-	if exportMode {
-		fmt.Fprintf(out, "export %s=\"%s\"\n", v.Name, value)
-	} else {
-		kv := fmt.Sprintf("%s=\"%s\"", v.Name, value)
-		fmt.Fprintf(out, "%-40s # %s\n", kv, v.Description)
-	}
 }
