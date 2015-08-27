33fb85864ec5d4dec042fa1646a7e269bde42ef7
Matt Aitchison <Matt@lanciv.com>
2015-08-27 11:57:21 -0500

    aws polling, env, comments, also fixed a bug in Create

--- aws/provider.go
+++ aws/provider.go
@@ -2,6 +2,8 @@ package aws
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/MattAitchison/env"
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,26 +12,37 @@ import (
 	"github.com/gliderlabs/hostctl/providers"
 )
 
-var envSet = env.NewEnvSet("aws")
-
 func init() {
 	providers.Register(&awsProvider{}, "aws")
 }
 
 type awsProvider struct {
 	client *ec2.EC2
+	config *aws.Config
 }
 
+// Setup ec2.Client using aws supported credentials (env, credential file)
 func (p *awsProvider) Setup() error {
-	p.client = ec2.New(nil)
+	region := os.Getenv("HOSTCTL_REGION")
+	if region == "" {
+		return fmt.Errorf("HOSTCTL_REGION required")
+	}
+	p.config = aws.NewConfig().WithRegion(region)
+	p.client = ec2.New(p.config)
 	return nil
 }
 
 func (p *awsProvider) Env() *env.EnvSet {
+	var envSet = env.NewEnvSet("aws")
+	envSet.Secret("AWS_ACCESS_KEY", "access key for AWS")
+	envSet.Secret("AWS_SECRET_KEY", "secret key for AWS")
+	envSet.String("AWS_AVAILABILITY_ZONE", "", "availability zone for AWS; eg: us-west-2a")
 	return envSet
 }
 
+// Create an instance based on a Host, poll until instance.Status=running
 func (p *awsProvider) Create(host providers.Host) error {
+	zone := os.Getenv("AWS_AVAILABILITY_ZONE")
 	res, err := p.client.RunInstances(&ec2.RunInstancesInput{
 		ImageId:      aws.String(host.Image),
 		MaxCount:     aws.Int64(1),
@@ -38,20 +51,18 @@ func (p *awsProvider) Create(host providers.Host) error {
 		KeyName:      aws.String(host.Keyname),
 		InstanceType: aws.String(host.Flavor),
 		Placement: &ec2.Placement{
-			AvailabilityZone: aws.String(host.Region),
+			AvailabilityZone: aws.String(zone),
 		},
 	})
 	if err != nil {
 		return err
 	}
-
-	if res != nil && len(res.Instances) > 0 {
+	if res == nil || len(res.Instances) == 0 {
 		return fmt.Errorf("no instances created")
 	}
+	instanceID := res.Instances[0].InstanceId
 	_, err = p.client.CreateTags(&ec2.CreateTagsInput{
-		Resources: []*string{
-			res.Instances[0].InstanceId,
-		},
+		Resources: []*string{instanceID},
 		Tags: []*ec2.Tag{
 			{
 				Key:   aws.String("Name"),
@@ -59,9 +70,13 @@ func (p *awsProvider) Create(host providers.Host) error {
 			},
 		},
 	})
-	return err
+	if err != nil {
+		return err
+	}
+	return p.pollFor(*instanceID, "running")
 }
 
+// Destroy the first instance with tag:Name=name
 func (p *awsProvider) Destroy(name string) error {
 	for id, host := range p.list(name) {
 		if host.Name == name {
@@ -71,11 +86,15 @@ func (p *awsProvider) Destroy(name string) error {
 	return nil
 }
 
+// destroy instance by ID polling until instance.State=terminated
 func (p *awsProvider) destroy(id string) error {
 	_, err := p.client.TerminateInstances(&ec2.TerminateInstancesInput{
 		InstanceIds: []*string{aws.String(id)},
 	})
-	return err
+	if err != nil {
+		return err
+	}
+	return p.pollFor(id, "terminated")
 }
 
 func (p *awsProvider) List(pattern string) []providers.Host {
@@ -86,6 +105,9 @@ func (p *awsProvider) List(pattern string) []providers.Host {
 	return hosts
 }
 
+// list all instances with tag:Name matching pattern.
+// Will NOT return any instances where PublicIpAddress is nil.
+// Map key will be set to the instanceID.
 func (p *awsProvider) list(pattern string) map[string]providers.Host {
 	res, err := p.client.DescribeInstances(&ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
@@ -101,11 +123,16 @@ func (p *awsProvider) list(pattern string) map[string]providers.Host {
 	hosts := make(map[string]providers.Host)
 	for i := range res.Reservations {
 		for _, instance := range res.Reservations[i].Instances {
+			// Note: an instance that is terminated/terminating will not have a public IP.
 			if instance != nil && instance.PublicIpAddress != nil {
 				id := *instance.InstanceId
 				hosts[id] = providers.Host{
-					Name: nametag(instance),
-					IP:   *instance.PublicIpAddress,
+					Name:    nametag(instance),
+					IP:      *instance.PublicIpAddress,
+					Region:  *instance.Placement.AvailabilityZone,
+					Image:   *instance.ImageId,
+					Keyname: *instance.KeyName,
+					Flavor:  *instance.InstanceType,
 				}
 			}
 		}
@@ -113,6 +140,7 @@ func (p *awsProvider) list(pattern string) map[string]providers.Host {
 	return hosts
 }
 
+// Get the first instance with tag:Name=name
 func (p *awsProvider) Get(name string) *providers.Host {
 	hosts := p.List(name)
 	for _, host := range hosts {
@@ -121,6 +149,33 @@ func (p *awsProvider) Get(name string) *providers.Host {
 		}
 	}
 	return nil
+}
+
+// pollFor will poll every 2 seconds for an instance by ID,
+// until instance.State=state, timeout or an error.
+func (p *awsProvider) pollFor(id string, state string) error {
+	timeout := time.After(1 * time.Minute)
+	for {
+		select {
+		case <-timeout:
+			return fmt.Errorf("aws provider: timed out wating for state: %s", state)
+		default:
+		}
+		res, err := p.client.DescribeInstances(&ec2.DescribeInstancesInput{
+			InstanceIds: []*string{aws.String(id)},
+		})
+		if err != nil {
+			return err
+		}
+		for i := range res.Reservations {
+			for _, instance := range res.Reservations[i].Instances {
+				if *instance.State.Name == state {
+					return nil
+				}
+			}
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func nametag(instance *ec2.Instance) string {
